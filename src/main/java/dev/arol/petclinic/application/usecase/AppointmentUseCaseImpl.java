8098e99c8f39e0126da4c35bc8bbb4e2cc16fadc
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-09-06 00:48:55 +0200

    feat: add PetRepository integration and validation in Appointment use case

--- src/main/java/dev/arol/petclinic/application/usecase/AppointmentUseCaseImpl.java
+++ src/main/java/dev/arol/petclinic/application/usecase/AppointmentUseCaseImpl.java
@@ -3,7 +3,9 @@ package dev.arol.petclinic.application.usecase;
 import dev.arol.petclinic.application.port.in.CreateAppointmentUseCase;
 import dev.arol.petclinic.application.port.in.GetAppointmentUseCase;
 import dev.arol.petclinic.application.port.out.AppointmentRepository;
+import dev.arol.petclinic.application.port.out.PetRepository;
 import dev.arol.petclinic.domain.model.Appointment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,9 +14,21 @@ import java.util.List;
 public class AppointmentUseCaseImpl implements CreateAppointmentUseCase, GetAppointmentUseCase {
 
     AppointmentRepository appointmentRepository;
+    private final PetRepository petRepository;
+
+    @Autowired
+    public AppointmentUseCaseImpl(AppointmentRepository appointmentRepository, PetRepository petRepository) {
+        this.appointmentRepository = appointmentRepository;
+        this.petRepository = petRepository;
+    }
 
     @Override
     public Appointment createAppointment(Appointment appointment) {
+        appointment.validateForCreation();
+
+        if (!petRepository.existsById(appointment.petId())) {
+            throw new IllegalArgumentException("Pet with ID " + appointment.petId() + " does not exist");
+        }
         return appointmentRepository.save(appointment);
     }
 
